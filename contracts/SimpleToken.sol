5451e6e4f14f5ba17be48446e5aa664d448477cc
Ken Lee <ken@flui.ai>
2019-09-04 22:47:59 +0900

    fix: apply prettier

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
 
 /**
  * @title SimpleToken
@@ -11,12 +11,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
  */
 contract SimpleToken is ERC20, ERC20Detailed {
 	uint8 public constant DECIMALS = 18;
-	uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
+	uint256 public constant INITIAL_SUPPLY = 10000 * (10**uint256(DECIMALS));
 
 	/**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-	constructor () public ERC20Detailed("SimpleToken", "SIM", DECIMALS) {
+	constructor() public ERC20Detailed('SimpleToken', 'SIM', DECIMALS) {
 		_mint(msg.sender, INITIAL_SUPPLY);
 	}
 }
