99603c0fadf16b582c1f524dc0f119b01fb0ea7b
Ken Lee <ken@flui.ai>
2019-09-04 22:36:06 +0900

    feat: seed project

---
+++ contracts/ERC721Token.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.6;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721Full.sol';
+
+contract ERC721Token {}
---
+++ contracts/SimpleBank.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.6;
+
+import '@openzeppelin/contracts/ownership/Ownable.sol';
+
+contract SimpleBank is Ownable {
+	mapping(address => uint256) private balance;
+	bool private bankOpen = true;
+
+	modifier bankOpened() {
+		require(isOwner() || bankOpen);
+		_;
+	}
+
+	function setBankState(bool _bankOpen) external onlyOwner {
+		bankOpen = _bankOpen;
+	}
+
+	function deposit(uint256 _amount) external payable bankOpened {
+		require(msg.value == _amount);
+		require(balance[msg.sender] + _amount > balance[msg.sender]);
+
+		balance[msg.sender] += _amount;
+	}
+
+	function withdraw(uint256 _amount) external bankOpened {
+		require(balance[msg.sender] >= _amount);
+
+		balance[msg.sender] -= _amount;
+		msg.sender.transfer(_amount);
+	}
+
+	function transfer(address _to, uint256 _amount) external bankOpened {
+		require(balance[msg.sender] >= _amount);
+		require(balance[_to] + _amount > balance[_to]);
+
+		balance[msg.sender] -= _amount;
+		balance[_to] += _amount;
+	}
+
+	function getBalance(address _user) external view bankOpened returns (uint256) {
+		return balance[_user];
+	}
+}
