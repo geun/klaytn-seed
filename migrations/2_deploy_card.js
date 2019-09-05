fafe2cb551aae4536c3085c63be2630dc3eae1e8
geun <geunbaelee@gmail.com>
2019-09-05 19:03:53 +0900

    feat: save abi and contract address

--- migrations/2_deploy_card.js
+++ migrations/2_deploy_card.js
@@ -4,11 +4,15 @@ const ERC721Card = artifacts.require('ERC721Card');
 module.exports = function(deployer) {
 	deployer.deploy(ERC721Card).then(() => {
 		const data = JSON.stringify({
-			blockAddress: ERC721Card.blockAddress,
 			contractAddress: ERC721Card.address
 		});
 		fs.writeFile('./contract.json', data, () =>
-			console.log(`migrate completed: ${ERC721Card.address}`)
+			console.log(`\n    Create file of contract address to json: ${ERC721Card.address}`)
+		);
+
+		const abi = JSON.stringify(ERC721Card._json.abi);
+		fs.writeFile('./abi.json', abi, () =>
+			console.log(`\n    Create file of abi file to json: ${ERC721Card.address}`)
 		);
 	});
 };
