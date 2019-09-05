5aaddf726bf6f4eada133c25401e6c61e9aa8be3
geun <geunbaelee@gmail.com>
2019-09-05 21:30:53 +0900

    feat: upload artifacts to azure storage

--- migrations/2_deploy_card.js
+++ migrations/2_deploy_card.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const AzureUploader = require('@flui/klaytn-uploader').AzureUploader;
 const ERC721Card = artifacts.require('ERC721Card');
 
 module.exports = function(deployer) {
@@ -6,13 +7,24 @@ module.exports = function(deployer) {
 		const data = JSON.stringify({
 			contractAddress: ERC721Card.address
 		});
-		fs.writeFile('./contract.json', data, () =>
-			console.log(`\n    Create file of contract address to json: ${ERC721Card.address}`)
-		);
+
+		if (!fs.existsSync('./artifacts')) {
+			fs.mkdirSync('./artifacts');
+			console.log(`\n    Create Artifacts`);
+		}
+
+		fs.writeFileSync('./artifacts/address.json', data);
+		console.log(`\n    Create file of contract address to json: ${ERC721Card.address}`);
 
 		const abi = JSON.stringify(ERC721Card._json.abi);
-		fs.writeFile('./abi.json', abi, () =>
-			console.log(`\n    Create file of abi file to json: ${ERC721Card.address}`)
-		);
+		fs.writeFileSync('./artifacts/abi.json', abi);
+		console.log(`\n    Create file of abi file to json: ${ERC721Card.address}`);
+
+		const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+		const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+		const uploader = new AzureUploader(accountName, accessKey);
+
+		const containerName = process.env.AZURE_STORAGE_CONTRACT_CONTAINER_NAME;
+		uploader.uploadArtifacts(ERC721Card._json.contractName, 'artifacts', containerName);
 	});
 };
