60bb25818ed7c8f2715cd614b1d7adc8c8772242
geun <geunbaelee@gmail.com>
2019-09-05 23:35:19 +0900

    feat: get Card count

--- contracts/ERC721Card.sol
+++ contracts/ERC721Card.sol
@@ -21,4 +21,8 @@ contract ERC721Card is ERC721 {
 		cards.push(Card(name, 1));
 		_mint(account, cardId); // Mint a new card
 	}
+
+	function getCardCount() public view returns(uint count) {
+		return cards.length;
+	}
 }
