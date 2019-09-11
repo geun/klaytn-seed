4304029b0f2ab01bde60523b662901f026e1e5b9
geun <geunbaelee@gmail.com>
2019-09-11 16:43:38 +0900

    feat: implement FLUIBank

--- contracts/FLUIBank.sol
+++ contracts/FLUIBank.sol
@@ -3,26 +3,41 @@ pragma solidity ^0.5.6;
 import '@openzeppelin/contracts/ownership/Ownable.sol';
 
 contract FLUIBank is Ownable {
-	mapping(address => uint256) private balance;
-	bool private bankOpen = true;
+    mapping(address => uint256) private balance;
+    bool private bankOpen = true;
 
-	modifier bankOpened() {
-		_;
-	}
+    modifier bankOpened() {
+        require(isOwner() || bankOpen);
+        _;
+    }
 
-	function setBankState(bool _bankOpen) external onlyOwner {
-	}
+    function setBankState(bool _bankOpen) external onlyOwner {
+        bankOpen = _bankOpen;
+    }
 
-	function deposit(uint256 _amount) external payable bankOpened {
-	}
+    function deposit(uint256 _amount) external payable bankOpened {
+        require(msg.value == _amount);
+        require(balance[msg.sender] + _amount > balance[msg.sender]);
 
-	function withdraw(uint256 _amount) external bankOpened {
-	}
+        balance[msg.sender] += _amount;
+    }
 
-	function transfer(address _to, uint256 _amount) external bankOpened {
-	}
+    function withdraw(uint256 _amount) external bankOpened {
+        require(balance[msg.sender] >= _amount);
 
-	function getBalance(address _user) external view bankOpened returns (uint256) {
-		return 0;
-	}
+        balance[msg.sender] -= _amount;
+        msg.sender.transfer(_amount);
+    }
+
+    function transfer(address _to, uint256 _amount) external bankOpened {
+        require(balance[msg.sender] >= _amount);
+        require(balance[_to] + _amount > balance[_to]);
+
+        balance[msg.sender] -= _amount;
+        balance[_to] += _amount;
+    }
+
+    function getBalance(address _user) external view bankOpened returns (uint256) {
+        return balance[_user];
+    }
 }
