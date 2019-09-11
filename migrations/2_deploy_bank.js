b93eacd5b529c27ec9f750d0f4a4e0bf48cde6e3
geun <geunbaelee@gmail.com>
2019-09-11 17:26:24 +0900

    feat: dump yaml config file

--- migrations/2_deploy_bank.js
+++ migrations/2_deploy_bank.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const yaml = require('js-yaml');
 const AzureUploader = require('@flui/klaytn-uploader').AzureUploader;
-const FLUBank = artifacts.require('FLUBank');
+const FLUIBank = artifacts.require('FLUIBank');
 
 module.exports = function(deployer) {
-	deployer.deploy(FLUBank).then(() => {
+	deployer.deploy(FLUIBank).then(() => {
 		const data = JSON.stringify({
-			contractAddress: FLUBank.address
+			contractAddress: FLUIBank.address
 		});
 
 		if (!fs.existsSync('./artifacts')) {
@@ -14,18 +15,37 @@ module.exports = function(deployer) {
 		}
 
 		fs.writeFileSync('./artifacts/address.json', data);
-		console.log(`\n    Create file of contract address to json: ${FLUBank.address}`);
+		console.log(`\n    Create file of contract address to json: ${FLUIBank.address}`);
 
-		const abi = JSON.stringify(FLUBank._json.abi);
+		const abi = JSON.stringify(FLUIBank._json.abi);
 		fs.writeFileSync('./artifacts/abi.json', abi);
-		console.log(`\n    Create file of abi file to json: ${FLUBank.address}`);
+		console.log(`\n    Create file of abi file to json: ${FLUIBank.address}`);
 
 		const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 		const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
 		const uploader = new AzureUploader(accountName, accessKey);
 
 		const containerName = process.env.AZURE_STORAGE_CONTRACT_CONTAINER_NAME;
-		uploader.uploadArtifacts(FLUBank._json.contractName, 'artifacts', containerName);
+		uploader
+			.uploadArtifacts(FLUIBank._json.contractName, 'artifacts', containerName)
+			.then(results => {
+				const [abiResult, addressResult] = results;
 
+				const envYaml = {
+					CONTRACT_ABI_JSON: abiResult.url.trim(),
+					CONTRACT_ADDRESS_JSON: addressResult.url.trim()
+				};
+
+				const doc = yaml.safeDump(envYaml, {
+					flowLevel: 0,
+					styles: {
+						'!!null': 'canonical' // dump null as ~
+					},
+					sortKeys: true // sort object keys
+				});
+
+				fs.writeFileSync('./artifacts/.env.contract', doc);
+				console.log(`\n    Create file contract env file to yaml`);
+			});
 	});
 };
